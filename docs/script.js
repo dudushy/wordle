7186f25794a3d35068b1b0a9e09c481eea9b7edc
dudushy <eduardotalarico1337@gmail.com>
2022-07-04 11:52:50 -0300

    Making the structure

--- docs/script.js
+++ docs/script.js
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+//* Vars
+var startButton = document.getElementById("startButton");
+
+//* Functions
+function setupGame() {
+    console.log("setupGame");
+}
+
+//? Main
+console.log(startButton);
+startButton.addEventListener("click", setupGame);
