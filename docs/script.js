d0a2601d19ad9a71a584e375e7e883167367b3bd
dudushy <eduardotalarico1337@gmail.com>
2022-07-04 14:53:27 -0300

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,11 +1,37 @@
 //* Vars
 var startButton = document.getElementById("startButton");
+var resetButton = document.getElementById("resetButton");
+var guessButton = document.getElementById("guessButton");
+var gameDiv = document.getElementById("gameDiv");
+var setupDiv = document.getElementById("setupDiv");
+
+var gameWord = null;
 
 //* Functions
 function setupGame() {
     console.log("setupGame");
+
+    setupDiv.style.display = "none"; // hide setup
+    gameDiv.style.display = "block"; // show game
+
+    gameWord = document.getElementById("gameWord").value;
+    console.log("gameWord: " + gameWord);
+}
+
+function resetGame() {
+    console.log("resetGame");
+
+    setupDiv.style.display = "block"; // show setup
+    gameDiv.style.display = "none"; // hide game
+
+    gameWord = null
+    document.getElementById("gameWord").value = null;
 }
 
 //? Main
-console.log(startButton);
+resetGame();
+// console.log(startButton);
 startButton.addEventListener("click", setupGame);
+
+// console.log(resetButton);
+resetButton.addEventListener("click", resetGame);
