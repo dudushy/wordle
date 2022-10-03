03ded70a090fd1df1e7fab6b62007da57f29de8c
dudushy <eduardotalarico1337@gmail.com>
2022-10-03 20:00:17 -0300

    SHIFT + ALT + F

--- docs/script.js
+++ docs/script.js
@@ -11,73 +11,73 @@ var regex = "qwertyuiopasdfghjklzxcvbnm";
 
 //* Functions
 function setupGame() {
-    console.log("setupGame");
+  console.log("setupGame");
 
-    console.log("gameWord.length", gameWord.length);
-    console.log("gameWordInput.value.length", gameWordInput.value.length);
-    console.log("gameWordInput.value", gameWordInput.value);
-    console.log("regex.includes(gameWordInput.value.toLowerCase())", regex.includes(gameWordInput.value.toLowerCase()));
+  console.log("gameWord.length", gameWord.length);
+  console.log("gameWordInput.value.length", gameWordInput.value.length);
+  console.log("gameWordInput.value", gameWordInput.value);
+  console.log("regex.includes(gameWordInput.value.toLowerCase())", regex.includes(gameWordInput.value.toLowerCase()));
 
-    if (gameWordInput.value.length == 5) {
-        gameWord = [];
+  if (gameWordInput.value.length == 5) {
+    gameWord = [];
 
-        for (const char of gameWordInput.value) {
-            console.log("char", char);
-            if (regex.includes(char.toLowerCase())) {
-                gameWord.push(char.toUpperCase());
-                console.log("gameWord.push", gameWord);
-            }
-        }
+    for (const char of gameWordInput.value) {
+      console.log("char", char);
+      if (regex.includes(char.toLowerCase())) {
+        gameWord.push(char.toUpperCase());
+        console.log("gameWord.push", gameWord);
+      }
     }
+  }
 
-    if (gameWord.length == 5) {
-        setupDiv.style.display = "none"; // hide setup
-        gameDiv.style.display = "block"; // show game
+  if (gameWord.length == 5) {
+    setupDiv.style.display = "none"; // hide setup
+    gameDiv.style.display = "block"; // show game
 
-        console.log("gameWord: " + gameWord);
-    }
+    console.log("gameWord: " + gameWord);
+  }
 }
 
 function resetGame() {
-    console.log("resetGame");
+  console.log("resetGame");
 
-    setupDiv.style.display = "block"; // show setup
-    gameDiv.style.display = "none"; // hide game
+  setupDiv.style.display = "block"; // show setup
+  gameDiv.style.display = "none"; // hide game
 
-    for (var i = 0; i < gameWord.length; i++) {
-        wordTableColumnSpan = document.querySelector("#wordBox" + (i + 1) + " > span");
-        console.log("wordTableColumnSpan", wordTableColumnSpan);
-        wordTableColumnSpan.innerHTML = "&nbsp;";
-    }
+  for (var i = 0; i < gameWord.length; i++) {
+    wordTableColumnSpan = document.querySelector("#wordBox" + (i + 1) + " > span");
+    console.log("wordTableColumnSpan", wordTableColumnSpan);
+    wordTableColumnSpan.innerHTML = "&nbsp;";
+  }
 
-    gameWordInput.value = null;
-    gameWord = [];
+  gameWordInput.value = null;
+  gameWord = [];
 
 }
 
 function addCharToGameWord(char) {
+  console.log("gameWord", gameWord);
+  console.log("char", char);
+
+  if (gameWord.length < 5 && regex.includes(char.toLowerCase())) {
+    gameWord.push(char.toUpperCase());
+
+    wordTableColumnSpan = document.querySelector("#wordBox" + gameWord.length + " > span");
+    console.log("wordTableColumnSpan", wordTableColumnSpan);
+
+    wordTableColumnSpan.innerHTML = char.toUpperCase();
+
     console.log("gameWord", gameWord);
-    console.log("char", char);
-
-    if (gameWord.length < 5 && regex.includes(char.toLowerCase())) {
-        gameWord.push(char.toUpperCase());
-
-        wordTableColumnSpan = document.querySelector("#wordBox" + gameWord.length + " > span");
-        console.log("wordTableColumnSpan", wordTableColumnSpan);
-
-        wordTableColumnSpan.innerHTML = char.toUpperCase();
-
-        console.log("gameWord", gameWord);
-    }
+  }
 }
 
 document.onkeypress = function (e) {
-    e = e || window.event;
-    console.log("key pressed: ", e);
+  e = e || window.event;
+  console.log("key pressed: ", e);
 
-    if (gameWord.length != 5) {
-        addCharToGameWord(e.key.substring(0));
-    }
+  if (gameWord.length != 5) {
+    addCharToGameWord(e.key.substring(0));
+  }
 };
 
 // document.addEventListener('keypress', (event) => {
