186459fa939cd8a39852df80ee7bcf93d9d999b0
dudushy <eduardotalarico1337@gmail.com>
2022-10-06 20:36:52 -0300

    `generate.sh`

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { TestComponent } from './test/test.component';
+import { MenuComponent } from './menu/menu.component';
+import { SettingsComponent } from './settings/settings.component';
+import { GameComponent } from './game/game.component';
+
+const routes: Routes = [
+  { path: 'test', component: TestComponent },
+  { path: 'menu', component: MenuComponent },
+  { path: 'settings', component: SettingsComponent },
+  { path: 'game', component: GameComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
+import { MenuComponent } from './menu/menu.component';
+import { SettingsComponent } from './settings/settings.component';
+import { GameComponent } from './game/game.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent,
+    MenuComponent,
+    SettingsComponent,
+    GameComponent
   ],
   imports: [
     BrowserModule,
